b76d21f4efde730c13f65d0695b5026183dbd8c1
salma hamdi <saalmahm51@gmail.com>
2024-12-19 19:00:11 +0100

    Préparer l'interface pour la page users de dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -8,40 +8,40 @@
 </head>
 <body class="bg-gray-100 font-sans">
 <header class="flex justify-between p-4 bg-white shadow">
-        <a href="/index.php" id="cars">
-            <img src="images/cars.gif" alt="">
-        </a>
-        <div class="lg:hidden" id="burger-icon">
-            <img src="/images/menu.png" alt="Menu">
+    <a href="/index.php" id="cars">
+        <img src="images/cars.gif" alt="">
+    </a>
+    <div class="lg:hidden" id="burger-icon">
+        <img src="/images/menu.png" alt="Menu">
+    </div>
+    <div id="sidebar"
+        class="shadow-xl fixed top-0 right-0 w-1/3 h-screen bg-gray-200 z-50 transform translate-x-full duration-300">
+        <div class="flex justify-end p-4">
+            <button id="close-sidebar" class="text-3xl">X</button>
         </div>
-        <div id="sidebar"
-            class="shadow-xl fixed top-0 right-0 w-1/3 h-screen bg-gray-200 z-50 transform translate-x-full duration-300">
-            <div class="flex justify-end p-4">
-                <button id="close-sidebar" class="text-3xl">X</button>
-            </div>
-            <div class="flex flex-col items-center space-y-4 text-white">
-                <a href="/index.php" class="text-black text-lg">Blog</a>
-                <a href="/pages/profile.php" class="text-black text-lg">Profile</a>
-                <a href="/pages/dashboard.php" class="text-black text-lg">Dashboard</a>
-                <a href="/pages/logout.php" class="text-red-500 text-lg">Log out</a>
-            </div>
+        <div class="flex flex-col items-center space-y-4 text-white">
+            <a href="/index.php" class="text-black text-lg">Blog</a>
+            <a href="/pages/profile.php" class="text-black text-lg">Profile</a>
+            <a href="/pages/dashboard.php" class="text-black text-lg">Dashboard</a>
+            <a href="/pages/logout.php" class="text-red-500 text-lg">Log out</a>
         </div>
-        <div class="hidden lg:flex justify-center space-x-4">
-            <ul class="flex items-center text-sm font-medium text-gray-400 mb-0">
-                <li>
-                    <a href="/index.php" class="hover:underline me-4 md:me-6">Blog</a>
-                </li>
-                <li>
-                    <a href="/pages/profile.php" class="hover:underline me-4 md:me-6">Profile</a>
-                </li>
-                <li>
-                    <a href="/pages/dashboard.php" class="hover:underline me-4 md:me-6">Dashboard</a>
-                </li>
-                <li>
-                    <a href="/pages/logout.php" class="text-red-500 hover:underline me-4 md:me-6">Log out</a>
-                </li>
-            </ul>
-        </div>
+    </div>
+    <div class="hidden lg:flex justify-center space-x-4">
+        <ul class="flex items-center text-sm font-medium text-gray-400 mb-0">
+            <li>
+                <a href="/index.php" class="hover:underline me-4 md:me-6">Blog</a>
+            </li>
+            <li>
+                <a href="/pages/profile.php" class="hover:underline me-4 md:me-6">Profile</a>
+            </li>
+            <li>
+                <a href="/pages/dashboard.php" class="hover:underline me-4 md:me-6">Dashboard</a>
+            </li>
+            <li>
+                <a href="/pages/logout.php" class="text-red-500 hover:underline me-4 md:me-6">Log out</a>
+            </li>
+        </ul>
+    </div>
 </header>
 <div class="flex">
     <aside class="fixed top-0 left-0 w-64 bg-gray-800 text-gray-200 h-screen z-40">
@@ -54,6 +54,48 @@
             <a href="./bord-tags.php"><button id="categories-btn" class="block w-full px-4 py-2 text-sm hover:bg-purple-600 text-purple-300">Categories</button></a>
         </nav>
     </aside>
+    <main class="ml-64 p-4 w-full">
+<div class="flex justify-between">
+<h1 class="text-2xl font-bold mb-4">users</h1>
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">+ Add</button>
+
+</div>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">Product name</th>
+                        <th scope="col" class="px-6 py-3">Color</th>
+                        <th scope="col" class="px-6 py-3">Category</th>
+                        <th scope="col" class="px-6 py-3">Price</th>
+                        <th scope="col" class="px-6 py-3">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Apple MacBook Pro 17"</th>
+                        <td class="px-6 py-4">Silver</td>
+                        <td class="px-6 py-4">Laptop</td>
+                        <td class="px-6 py-4">$2999</td>
+                        <td class="px-6 py-4">
+                        <a href="#" class="font-medium text-blue-600 hover:underline">Edit</a>
+                        <a href="#" class="font-medium text-red-600 hover:underline">Delete</a>                        </td>
+                    </tr>
+                    <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Microsoft Surface Pro</th>
+                        <td class="px-6 py-4">White</td>
+                        <td class="px-6 py-4">Laptop PC</td>
+                        <td class="px-6 py-4">$1999</td>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 hover:underline">Edit</a>
+                            <a href="#" class="font-medium text-red-600 hover:underline">Delete</a>
+
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </main>
 </div>
 </body>
 </html>
